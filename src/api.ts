ca34ab814bdecf6c10a8717a01a29d8ddd8cad54
Tanatorn Jitsatsawan <156393142+6530200207@users.noreply.github.com>
2026-04-03 04:15:56 +0700

    Change API base URL from 127.0.0.1 to localhost

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000'
+const API_BASE_URL = 'http://localhost:8000'
 // const API_BASE_URL = 'https://project-wah5.onrender.com'
 import type { User } from './App'
 import { useFetchWithLoading } from './components/fetchWithLoading'
